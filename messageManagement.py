cb5d9181927cecebbaddc7425e29ff06bab61eeb
Antonin Geslin Dugué <latouillette@MacBook-Air-de-Antonin.local>
2023-03-23 13:27:32 +0100

    gestion des messages

---
+++ messageManagement.py
@@ -0,0 +1,24 @@
+class MessageManagement():
+
+
+    def __init__(self, user, password, database, host='localhost'):
+        self.user = user
+        self.password = password
+        self.database = database
+        self.host = host
+        self.connexion = mysql.connector.connect(user=self.user, password=self.password, host=self.host, database=self.database)
+        self.cursor = self.connexion.cursor()
+
+
+
+    def addMessageToBdd(self, user, message):
+        query = ("INSERT INTO messages (user, message) VALUES (%s, %s)")
+        values = (user, message)
+        self.cursor.execute(query, values)
+        self.connexion.commit()
+        return self.cursor.lastrowid
+
+    def getAllMessages(self):
+        query = ("SELECT user, message FROM messages")
+        self.cursor.execute(query)
+        return(self.cursor.fetchall())
